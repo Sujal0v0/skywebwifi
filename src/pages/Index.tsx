50c1497965f6b72dc09fc9a5bc99b2eb6cd0f11a
Sujal0v0 <bajracharyasujal80@gmail.com>
2025-09-12 20:12:14 +0545

    fix: cloudflare

--- src/pages/Index.tsx
+++
@@ -1,8 +0,0 @@
-// Redirect to Home page since we're using the new routing structure
-import { Navigate } from 'react-router-dom';
-
-const Index = () => {
-  return <Navigate to="/" replace />;
-};
-
-export default Index;
