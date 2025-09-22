c15de15d1f905896607de893c40a484d9319cd01
Rajesh Shrestha <rajeshrestha@gmail.com>
2025-09-22 22:23:54 +0545

    update content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
       <Hero />
       <WhyChooseUs />
       <ProofSection />
-      <BenefitsSection />
+      {/* <BenefitsSection /> */}
       <HowItWorks />
       <FeaturedCaseStudy />
       <Testimonials />
